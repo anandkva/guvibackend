91cc62ab60239079f37b13cdb24f1d36f41cad8a
Anand <anand.kva00@gmail.com>
2021-08-24 16:26:23 +0530

    port url change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 const db = require('./database')
 const userRouter = require('./router/user.routes')
 
